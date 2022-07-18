e679f1ca98cb44772ddbd54080e4bf54162c6c80
Vincent <vpark45@gmail.com>
2022-07-18 22:33:47 +0000

    Test prop_primesArePrime

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,13 @@ prop_validPrimesOnly val = if val < 0 || val >= length primes
                            else isJust result
   where result = isPrime val
 
+prop_primesArePrime val = if result == Just True
+                          then length divisors == 0
+                          else True
+  where result   = isPrime val
+        divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
 main :: IO ()
 main = do
   quickCheck prop_validPrimesOnly
+  quickCheck prop_primesArePrime
