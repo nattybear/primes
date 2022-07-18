6de6d98fe5bebe89fd40fb9d2590520d4bd3a0a3
Vincent <vpark45@gmail.com>
2022-07-18 22:22:53 +0000

    Add isPrime function

--- src/Primes.hs
+++ src/Primes.hs
@@ -7,3 +7,8 @@ sieve :: [Int] -> [Int]
 sieve [] = []
 sieve (nextPrime:rest) = nextPrime : sieve noFactors
   where noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
+
+isPrime :: Int -> Maybe Bool
+isPrime n | n < 0              = Nothing
+          | n >= length primes = Nothing
+          | otherwise          = Just (n `elem` primes)
