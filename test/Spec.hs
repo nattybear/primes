9418281ff7dae39cc532e6920cf1b69c37320637
Vincent <vpark45@gmail.com>
2022-07-18 22:30:10 +0000

    Test isPrime

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,12 @@
-import Test.QuickCheck
+import Data.Maybe
 import Primes
+import Test.QuickCheck
+
+prop_validPrimesOnly val = if val < 0 || val >= length primes
+                           then result == Nothing
+                           else isJust result
+  where result = isPrime val
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_validPrimesOnly
