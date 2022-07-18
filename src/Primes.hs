a3fe0af554bd67ab0705602734ca5cb0923e778b
Vincent <vpark45@gmail.com>
2022-07-18 22:46:30 +0000

    Add unsafePrimeFactors function

--- src/Primes.hs
+++ src/Primes.hs
@@ -12,3 +12,11 @@ isPrime :: Int -> Maybe Bool
 isPrime n | n < 2              = Nothing
           | n >= length primes = Nothing
           | otherwise          = Just (n `elem` primes)
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:primes) =
+  if n `mod` next == 0
+  then next : unsafePrimeFactors (n `div` next) (next:primes)
+  else unsafePrimeFactors n primes
